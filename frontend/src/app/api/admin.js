a68666763edee0b9dadb351d12b9b76f4166d695
Md. Jehadul Karim <sabit.jehadul.karim@gmail.com>
2024-03-27 03:16:13 +0600

    feat: admin user controls

--- frontend/src/app/api/admin.js
+++ frontend/src/app/api/admin.js
@@ -1,11 +1,27 @@
 import { api_url } from ".";
 import axios from "axios";
 
+export const getUsers = async () =>
+  axios.get(api_url(`/users`)).then((res) => res.data);
+
+export const getRoles = async () =>
+  axios.get(api_url(`/users/roles`)).then((res) => res.data);
+
+export const changeUserRole = async (id, role) =>
+  axios
+    .put(api_url(`/users/${id}/roles`), { role_name: role })
+    .then((res) => res.data);
+
+export const editUser = async (id, username, email, name, active, banned) =>
+  axios
+    .put(api_url(`/users/${id}`), { username, email, name, active, banned })
+    .then((res) => res.data);
+
+export const deleteUser = async (id) =>
+  axios.delete(api_url(`/users/${id}`)).then((res) => res.data);
+
 export const addDoctorRequest = async (email) =>
   axios.post(api_url("/admin/add-doctor"), { email }).then((res) => res.data);
-
-export const getUsers = async (page) =>
-  axios.get(api_url(`/admin/users?page=${page}`)).then((res) => res.data);
 
 export const changeUserBannedStatus = async (id, banned) =>
   axios(api_url(`/admin/user/${id}`), {
@@ -17,4 +33,6 @@ export const filterAndAnalyze = async (data) =>
   axios.post(api_url("/admin/filter"), data).then((res) => res.data);
 
 export const addUser = async (username, email, password, name, send_email) =>
-  axios.post(api_url("/users"), { username, email, password, name, send_email}).then((res) => res.data);
+  axios
+    .post(api_url("/users"), { username, email, password, name, send_email })
+    .then((res) => res.data);
--- frontend/src/app/system-admin/Users.js
+++ frontend/src/app/system-admin/Users.js
@@ -2,7 +2,15 @@ import { useState } from "react";
 import { useQuery } from "../user/OwnedDocuments";
 import { useEffect } from "react";
 import toast from "react-hot-toast";
-import { changeUserBannedStatus, getUsers } from "../api/admin";
+import { Modal } from "react-bootstrap";
+import {
+  changeUserBannedStatus,
+  changeUserRole,
+  deleteUser,
+  editUser,
+  getRoles,
+  getUsers,
+} from "../api/admin";
 import { formatDateFromTimestamp } from "../user/SharedByMe";
 
 export default function Users() {
@@ -13,23 +21,33 @@ export default function Users() {
   const [first, setFirst] = useState(true);
   const [last, setLast] = useState(true);
 
-  // useEffect(() => {
-  //   (async () => {
-  //     toast.promise(
-  //       // getUsers(page).then((res) => {
-  //       //   setUsers(res.content);
-  //       //   setFirst(res.first);
-  //       //   setLast(res.last);
-  //       // }),
-  //       () => {},
-  //       {
-  //         loading: "Loading users",
-  //         success: "Loaded users",
-  //         error: "Failed loading users",
-  //       }
-  //     );
-  //   })();
-  // }, [page]);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [selectedDeletedUser, setSelectedDeletedUser] = useState(null);
+
+  const [roles, setRoles] = useState([
+    "SYSTEM_ADMIN",
+    "STS_MANAGER",
+    "LANDFILL_MANAGER",
+    "UNASSIGNED",
+  ]);
+
+  useEffect(() => {
+    (async () => {
+      toast.promise(
+        getUsers().then((res) => {
+          setUsers(res);
+          // setFirst(res.first);
+          // setLast(res.last);
+        }),
+        {
+          loading: "Loading users",
+          success: "Loaded users",
+          error: "Failed loading users",
+        }
+      );
+    })();
+    getRoles().then((res) => setRoles(res.map((r) => r.name)));
+  }, []);
 
   return (
     <div>
@@ -92,15 +110,49 @@ export default function Users() {
                   <tbody>
                     {users.map((user) => (
                       <tr
-                        key={user.username}
-                        className={
-                          user.notification ? " text-danger" : "text-dark"
-                        }
+                        key={user.user_id}
+                        className={user.banned ? " text-danger" : "text-dark"}
                       >
                         <td> {user.username} </td>
                         <td> {user.email} </td>
                         <td> {user.name} </td>
-                        <td> {user.role} </td>
+                        <td>
+                          <select
+                            className="form-control form-control-sm"
+                            value={user?.role_name}
+                            onChange={(e) => {
+                              toast.promise(
+                                changeUserRole(
+                                  user.user_id,
+                                  e.target.value
+                                ).then((e) => {
+                                  setUsers(
+                                    users.map((u) => {
+                                      if (u.user_id === user.user_id) {
+                                        return {
+                                          ...user,
+                                          role_name: e.role_name,
+                                        };
+                                      }
+                                      return u;
+                                    })
+                                  );
+                                }),
+                                {
+                                  loading: "Changing role",
+                                  success: "Changed role",
+                                  error: "Failed changing role",
+                                }
+                              );
+                            }}
+                          >
+                            {roles.map((role) => (
+                              <option key={role} value={role}>
+                                {role}
+                              </option>
+                            ))}
+                          </select>
+                        </td>
                         <td>
                           {" "}
                           {user.active ? (
@@ -123,25 +175,19 @@ export default function Users() {
                             className="btn btn-outline-dark btn-sm"
                             onClick={(e) => {
                               e.preventDefault();
-                              toast.promise(
-                                // changeUserActiveStatus(
-                                //   user.id,
-                                //   !user.active
-                                // ).then((res) => {
-                                //   user.active = !user.active;
-                                //   setUsers([...users]);
-                                // }),
-                                () => {},
-                                {
-                                  loading: "Changing user active status",
-                                  success: "Changed user active status",
-                                  error: "Failed changing user active status",
-                                }
-                              );
+                              setSelectedUser(user);
                             }}
                           >
                             Edit
-                            </button>{" "}
+                          </button>{" "}
+                          <button
+                            className="btn btn-outline-danger btn-sm"
+                            onClick={(e) => {
+                              setSelectedDeletedUser(user);
+                            }}
+                          >
+                            Delete
+                          </button>
                         </td>
                       </tr>
                     ))}
@@ -152,6 +198,193 @@ export default function Users() {
           </div>
         </div>
       </div>
+      <Modal
+        show={selectedDeletedUser}
+        onHide={() => setSelectedDeletedUser(null)}
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>
+            Are you sure you want to delete "{selectedDeletedUser?.username}"?
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="row">
+            <div className="col-md-12 text-right">
+              <button
+                className="btn btn-outline-danger mr-2"
+                onClick={() => setSelectedDeletedUser(null)}
+              >
+                Cancel
+              </button>
+              <button
+                className="btn btn-danger"
+                onClick={() => {
+                  toast.promise(
+                    deleteUser(selectedDeletedUser.user_id).then((e) => {
+                      setUsers(
+                        users.filter(
+                          (u) => u.user_id !== selectedDeletedUser.user_id
+                        )
+                      );
+                      setSelectedDeletedUser(null);
+                    }),
+                    {
+                      loading: "Deleting user",
+                      success: "Deleted user",
+                      error: "Failed deleting user",
+                    }
+                  );
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+
+      <Modal
+        show={selectedUser}
+        onHide={() => setSelectedUser(null)}
+        centered
+        size="md"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>
+            Change "{selectedUser?.username}" Information
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="row">
+            <div className="col-md-6">
+              <div className="form-group">
+                <label>Username</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={selectedUser?.username}
+                  onChange={(e) => {
+                    setSelectedUser({
+                      ...selectedUser,
+                      username: e.target.value,
+                    });
+                  }}
+                />
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="form-group">
+                <label>Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  value={selectedUser?.email}
+                  onChange={(e) => {
+                    setSelectedUser({
+                      ...selectedUser,
+                      email: e.target.value,
+                    });
+                  }}
+                />
+              </div>
+            </div>
+            <div className="col-md-12">
+              <div className="form-group">
+                <label>Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={selectedUser?.name}
+                  onChange={(e) => {
+                    setSelectedUser({
+                      ...selectedUser,
+                      name: e.target.value,
+                    });
+                  }}
+                />
+              </div>
+            </div>
+            <div className="col-md-12">
+              <div className="form-group">
+                <div className="form-check">
+                  <label className="form-check-label text-dark">
+                    <input
+                      type="checkbox"
+                      className="form-check-input"
+                      checked={selectedUser?.active}
+                      onChange={(e) => {
+                        setSelectedUser({
+                          ...selectedUser,
+                          active: e.target.checked,
+                        });
+                      }}
+                    />
+                    <i className="input-helper"></i> Active (User has logged in
+                    and changed password)
+                  </label>
+                </div>
+                <div className="form-check">
+                  <label className="form-check-label text-danger">
+                    <input
+                      type="checkbox"
+                      className="form-check-input"
+                      checked={selectedUser?.banned}
+                      onChange={(e) => {
+                        setSelectedUser({
+                          ...selectedUser,
+                          banned: e.target.checked,
+                        });
+                      }}
+                    />
+                    <i className="input-helper"></i> Banned (User cannot log in)
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-12 text-right">
+              <button
+                className="btn btn-outline-danger mr-2"
+                onClick={() => setSelectedUser(null)}
+              >
+                Cancel
+              </button>
+              <button
+                className="btn btn-primary"
+                onClick={async () => {
+                  toast.promise(
+                    editUser(
+                      selectedUser.user_id,
+                      selectedUser.username,
+                      selectedUser.email,
+                      selectedUser.name,
+                      selectedUser.active,
+                      selectedUser.banned
+                    ).then((e) => {
+                      setUsers(
+                        users.map((u) => {
+                          if (u.user_id === selectedUser.user_id) {
+                            return selectedUser;
+                          }
+                          return u;
+                        })
+                      );
+                      setSelectedUser(null);
+                    }),
+                    {
+                      loading: "Saving changes",
+                      success: "Saved changes",
+                      error: "Failed saving changes",
+                    }
+                  );
+                }}
+              >
+                Save
+              </button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
