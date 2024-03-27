3d467bdb0fd1a055cabcb84d254134b63812d768
Md. Jehadul Karim <sabit.jehadul.karim@gmail.com>
2024-03-28 02:58:01 +0600

    feat: admin vehicle functions

---
+++ frontend/src/app/system-admin/Vehicles.js
@@ -0,0 +1,472 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
+import { Modal } from "react-bootstrap";
+import { Typeahead } from "react-bootstrap-typeahead";
+import {
+  addLandfill,
+  addManagerToLandfill,
+  addVehicle,
+  deleteLandfill,
+  deleteVehicle,
+  getLandfills,
+  getManagersOfLandfill,
+  getUsers,
+  getVehicles,
+  removeManagerFromLandfill,
+  updateLandfill,
+  updateVehicle,
+} from "../api/admin";
+import { USER_ROLES } from "../App";
+
+const VEHICLE_TYPES = [
+  "Open Truck",
+  "Closed Truck",
+  "Compactor",
+  "Container Carrier",
+];
+
+const defaultVehicle = {
+  vehicle_id: 0,
+  registration: "",
+  type: VEHICLE_TYPES[0],
+  capacity: 0.0,
+  disabled: false,
+  fuel_cost_per_km_loaded: 0.0,
+  fuel_cost_per_km_unloaded: 0.0,
+  landfill_id: null,
+};
+
+export default function Vehicles() {
+  // const [query, location] = useQuery();
+  // const page = (Number.parseInt(query.get("page")) || 1) - 1;
+
+  const [vehicles, setVehicles] = useState([]);
+  const [first, setFirst] = useState(true);
+  const [last, setLast] = useState(true);
+
+  const [selectedEditVehicle, setSelectedEditVehicle] = useState(null);
+  const [selectedDeleteVehicle, setSelectedDeleteVehicle] = useState(null);
+  const [selectedLandfillManagers, setSelectedLandfillManagers] =
+    useState(null);
+
+  const [landfills, setLandfills] = useState([]);
+
+  useEffect(() => {
+    toast.promise(
+      getVehicles().then((v) => {
+        setVehicles(v);
+      }),
+      {
+        loading: "Loading Landfills",
+        success: "Loaded Landfills",
+        error: "Failed loading Landfills",
+      }
+    );
+    getLandfills().then((lfs) => {
+      setLandfills(lfs);
+    });
+  }, []);
+
+  const closeManagerModal = () =>
+    setSelectedLandfillManagers((m) => {
+      setVehicles(
+        vehicles.map((s) =>
+          s.landfill_id === m.landfill_id
+            ? { ...s, manager_count: `${m.managers.length}` }
+            : s
+        )
+      );
+      return null;
+    });
+
+  return (
+    <div>
+      <div className="page-header">
+        <h3 className="page-title"> Vehicles </h3>
+        <nav aria-label="breadcrumb">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item">
+              <a href="!#" onClick={(event) => event.preventDefault()}>
+                Landfills
+              </a>
+            </li>
+            <li className="breadcrumb-item active" aria-current="page">
+              Vehicles
+            </li>
+          </ol>
+        </nav>
+      </div>
+
+      <div className="row">
+        <div className="col-lg-12 grid-margin stretch-card">
+          <div className="card">
+            <div className="card-body">
+              <h4 className="card-title">
+                Vehicles
+                <span className="float-right">
+                  <button
+                    className={
+                      "btn btn-outline-primary btn-sm icon-btn" +
+                      (first ? " invisible" : "")
+                    }
+                  >
+                    <i className="mdi mdi-arrow-left-thick"></i>
+                    <span>Previous</span>
+                  </button>
+                  <button
+                    className={
+                      "btn btn-outline-primary btn-sm icon-btn" +
+                      (last ? " invisible" : "")
+                    }
+                  >
+                    <span>Next</span>
+                    <i className="mdi mdi-arrow-right-thick"></i>
+                  </button>
+                  <button
+                    className={"btn btn-outline-success btn-sm icon-btn"}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setSelectedEditVehicle(defaultVehicle);
+                    }}
+                  >
+                    <i className="mdi mdi-plus mr-2"></i>
+                    <span>Add New</span>
+                  </button>
+                </span>
+              </h4>
+              <p className="card-description">
+                <table className="table table-outline table-hover">
+                  <thead>
+                    <tr>
+                      <th> Registration # </th>
+                      <th> Type </th>
+                      <th> Capacity </th>
+                      <th> Cost/km (loaded / unloaded) </th>
+                      <th> Disabled </th>
+                      <th> Associated Landfill </th>
+                      <th> Action </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {vehicles.map((s) => (
+                      <tr key={s.vehicle_id} className={"text-dark"}>
+                        <td> {s.registration} </td>
+                        <td> {s.type} </td>
+                        <td> {s.capacity} </td>
+                        <td>
+                          {s.fuel_cost_per_km_loaded} /{" "}
+                          {s.fuel_cost_per_km_unloaded}
+                        </td>
+                        <td>
+                          <span
+                            className={
+                              s.disabled ? " text-danger" : "text-success"
+                            }
+                          >
+                            {" "}
+                            {s.disabled ? "Yes" : "No"}{" "}
+                          </span>
+                        </td>
+                        <td>
+                          <span
+                            className={
+                              "mdi mdi-delete-variant mr-2 " +
+                              (s.landfill_id === null
+                                ? " text-danger"
+                                : "text-dark")
+                            }
+                          >
+                            {" "}
+                            {s.landfill_id === null
+                              ? "No Landfill"
+                              : landfills.find(
+                                  (l) => l.landfill_id === s.landfill_id
+                                )?.name}{" "}
+                          </span>
+                        </td>
+                        <td>
+                          {" "}
+                          <button
+                            className="btn btn-outline-dark btn-sm"
+                            onClick={(e) => {
+                              e.preventDefault();
+                              setSelectedEditVehicle(s);
+                            }}
+                          >
+                            Edit
+                          </button>{" "}
+                          <button
+                            className="btn btn-outline-danger btn-sm"
+                            onClick={(e) => {
+                              setSelectedDeleteVehicle(s);
+                            }}
+                          >
+                            Delete
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Modal
+        show={selectedDeleteVehicle}
+        onHide={() => setSelectedDeleteVehicle(null)}
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>
+            Are you sure you want to delete Vehicle - "
+            {selectedDeleteVehicle?.registration}"?
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="row">
+            <div className="col-md-12 text-right">
+              <button
+                className="btn btn-outline-danger mr-2"
+                onClick={() => setSelectedDeleteVehicle(null)}
+              >
+                Cancel
+              </button>
+              <button
+                className="btn btn-danger"
+                onClick={() => {
+                  toast.promise(
+                    deleteVehicle(selectedDeleteVehicle.vehicle_id).then(() => {
+                      setVehicles(
+                        vehicles.filter(
+                          (v) =>
+                            v.vehicle_id !== selectedDeleteVehicle.vehicle_id
+                        )
+                      );
+                      setSelectedDeleteVehicle(null);
+                    }),
+                    {
+                      loading: "Deleting Vehicle",
+                      success: "Deleted Vehicle",
+                      error: "Failed deleting Vehicle",
+                    }
+                  );
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+
+      <Modal
+        show={selectedEditVehicle}
+        onHide={() => setSelectedEditVehicle(null)}
+        centered
+        size="md"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>
+            {selectedEditVehicle?.vehicle_id === 0
+              ? "Add Vehicle"
+              : `Edit Vehicle - "${selectedEditVehicle?.registration}"`}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="row">
+            <div className="col-md-6">
+              <div className="form-group">
+                <label>Registration</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={selectedEditVehicle?.registration}
+                  onChange={(e) => {
+                    setSelectedEditVehicle({
+                      ...selectedEditVehicle,
+                      registration: e.target.value,
+                    });
+                  }}
+                />
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="form-group">
+                <label>Type</label>
+                <select
+                  className="form-control"
+                  value={selectedEditVehicle?.type}
+                  onChange={(e) => {
+                    setSelectedEditVehicle({
+                      ...selectedEditVehicle,
+                      type: e.target.value,
+                    });
+                  }}
+                >
+                  {VEHICLE_TYPES.map((t) => (
+                    <option key={t} value={t}>
+                      {t}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+
+            <div className="col-md-6">
+              <div className="form-group">
+                <label>Capacity</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  value={selectedEditVehicle?.capacity}
+                  onChange={(e) => {
+                    setSelectedEditVehicle({
+                      ...selectedEditVehicle,
+                      capacity: Number.parseFloat(e.target.value),
+                    });
+                  }}
+                />
+              </div>
+            </div>
+
+            <div className="col-md-6">
+              <div className="form-group">
+                <label>Disabled</label>
+                <select
+                  className="form-control"
+                  value={selectedEditVehicle?.disabled}
+                  onChange={(e) => {
+                    setSelectedEditVehicle({
+                      ...selectedEditVehicle,
+                      disabled: e.target.value === "true",
+                    });
+                  }}
+                >
+                  <option value={false}>No</option>
+                  <option value={true}>Yes</option>
+                </select>
+              </div>
+            </div>
+
+            <div className="col-md-6">
+              <div className="form-group">
+                <label>Fuel Cost per km (Loaded)</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  value={selectedEditVehicle?.fuel_cost_per_km_loaded}
+                  onChange={(e) => {
+                    setSelectedEditVehicle({
+                      ...selectedEditVehicle,
+                      fuel_cost_per_km_loaded: Number.parseFloat(
+                        e.target.value
+                      ),
+                    });
+                  }}
+                />
+              </div>
+            </div>
+
+            <div className="col-md-6">
+              <div className="form-group">
+                <label>Fuel Cost per km (Unloaded)</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  value={selectedEditVehicle?.fuel_cost_per_km_unloaded}
+                  onChange={(e) => {
+                    setSelectedEditVehicle({
+                      ...selectedEditVehicle,
+                      fuel_cost_per_km_unloaded: Number.parseFloat(
+                        e.target.value
+                      ),
+                    });
+                  }}
+                />
+              </div>
+            </div>
+
+            <div className="col-md-12">
+              <div className="form-group">
+                <label>Landfill</label>
+                <select
+                  className="form-control"
+                  value={selectedEditVehicle?.landfill_id}
+                  onChange={(e) => {
+                    setSelectedEditVehicle({
+                      ...selectedEditVehicle,
+                      landfill_id: e.target.value,
+                    });
+                  }}
+                >
+                  <option value={null}>No Landfill</option>
+                  {landfills.map((l) => (
+                    <option key={l.landfill_id} value={l.landfill_id}>
+                      {l.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+
+            <div className="col-md-12 text-right">
+              <button
+                className="btn btn-outline-danger mr-2"
+                onClick={() => setSelectedEditVehicle(null)}
+              >
+                Cancel
+              </button>
+              <button
+                className="btn btn-primary"
+                onClick={async () => {
+                  toast.promise(
+                    selectedEditVehicle?.vehicle_id === 0
+                      ? addVehicle(
+                          selectedEditVehicle.registration,
+                          selectedEditVehicle.type,
+                          selectedEditVehicle.capacity,
+                          selectedEditVehicle.disabled,
+                          selectedEditVehicle.fuel_cost_per_km_loaded,
+                          selectedEditVehicle.fuel_cost_per_km_unloaded,
+                          selectedEditVehicle.landfill_id
+                        ).then((e) => {
+                          setVehicles([...vehicles, e]);
+                          setSelectedEditVehicle(null);
+                        })
+                      : updateVehicle(
+                          selectedEditVehicle.vehicle_id,
+                          selectedEditVehicle.registration,
+                          selectedEditVehicle.type,
+                          selectedEditVehicle.capacity,
+                          selectedEditVehicle.disabled,
+                          selectedEditVehicle.fuel_cost_per_km_loaded,
+                          selectedEditVehicle.fuel_cost_per_km_unloaded,
+                          selectedEditVehicle.landfill_id
+                        ).then((e) => {
+                          setVehicles(
+                            vehicles.map((v) =>
+                              v.vehicle_id === e.vehicle_id ? e : v
+                            )
+                          );
+                          setSelectedEditVehicle(null);
+                        }),
+                    {
+                      loading: "Saving Vehicle",
+                      success: "Saved Vehicle",
+                      error: "Failed saving Vehicle",
+                    }
+                  );
+                }}
+              >
+                Save
+              </button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+}
