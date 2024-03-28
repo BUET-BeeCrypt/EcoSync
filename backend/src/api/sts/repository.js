e9b7c9d0b302341663aded727c2eb88afe2e0aac
Hasan Masum <1805052@ugrad.cse.buet.ac.bd>
2024-03-28 20:59:27 +0600

    Refactor STS repository module

--- backend/src/api/sts/repository.js
+++ backend/src/api/sts/repository.js
@@ -1,6 +1,8 @@
 const { get } = require("../../../app");
 const pool = require(`../../db/pool`);
 
+modules = {}
+
 /*
 CREATE TABLE public."STS"
 (
@@ -53,7 +55,7 @@ CREATE TABLE public."STS_Entry"
 );
 */
 
-const createSTS = async (sts) => {
+modules.createSTS = async (sts) => {
   const { zone_no,ward_no,name,location,latitude,longitude,capacity,dump_area,coverage_area } = sts;
   const query = `INSERT INTO public."STS" (zone_no,ward_no,name,location,latitude,longitude,capacity,dump_area,coverage_area) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`;
   const values = [zone_no,ward_no,name,location,latitude,longitude,capacity,dump_area,coverage_area];
@@ -61,13 +63,13 @@ const createSTS = async (sts) => {
   return result.rows[0];
 };
 
-const existsSTS = async (sts_id) => {
+modules.existsSTS = async (sts_id) => {
   const query = `SELECT 1 FROM public."STS" WHERE sts_id = $1`;
   const result = await pool.query(query, [sts_id]);
   return result.rows.length > 0;
 };
 
-const getSTSs = async (limit, offset) => {
+modules.getSTSs = async (limit, offset) => {
   const query = `SELECT *,
 	(SELECT COUNT(*) FROM public."STS_Manager" sm WHERE sm.sts_id = s.sts_id) as manager_count,
 	(SELECT COALESCE(SUM(volume),0) FROM public."STS_Entry" se WHERE se.sts_id = s.sts_id) as amount
@@ -77,7 +79,7 @@ const getSTSs = async (limit, offset) => {
   return result.rows;
 };
 
-const getSTS = async (sts_id) => {
+modules.getSTS = async (sts_id) => {
   // json_agg(json_build_object('user_id', user_id, 'username', username, 'email', email, 'role', role)) FROM public."User" WHERE user_id IN (SELECT user_id FROM public."STS_Manager" WHERE sts_id = $1)
   const query = `SELECT * ,
 	(SELECT COUNT(*) FROM public."STS_Manager" WHERE sts_id = $1) as manager_count,
@@ -90,7 +92,7 @@ const getSTS = async (sts_id) => {
   return result.rows[0];
 };
 
-const updateSTS = async (sts_id, sts) => {
+modules.updateSTS = async (sts_id, sts) => {
   const { zone_no,ward_no,name,location,latitude,longitude,capacity,dump_area,coverage_area } = sts;
   const query = `UPDATE public."STS" SET zone_no = $1, ward_no = $2, name = $3, location = $4, latitude = $5, longitude = $6, capacity = $7, dump_area = $8, coverage_area = $9 WHERE sts_id = $10 RETURNING *`;
   const values = [zone_no,ward_no,name,location,latitude,longitude,capacity,dump_area,coverage_area, sts_id];
@@ -101,56 +103,63 @@ const updateSTS = async (sts_id, sts) => {
   return result.rows[0];
 };
 
-const deleteSTS = async (sts_id) => {
+modules.deleteSTS = async (sts_id) => {
   const query = `DELETE FROM public."STS" WHERE sts_id = $1`;
   await pool.query(query, [sts_id]);
 };
 
-const addManagerToSTS = async (sts_id, user_id) => {
+modules.addManagerToSTS = async (sts_id, user_id) => {
   const query = `INSERT INTO public."STS_Manager" (sts_id, user_id) VALUES ($1, $2) RETURNING *`;
   const values = [sts_id, user_id];
   result = await pool.query(query, values);
   return result.rows[0];
 };
 
-const getManagersOfSTS = async (sts_id) => {
+modules.getManagersOfSTS = async (sts_id) => {
   const query = `SELECT user_id, "name", username, email
   FROM public."User" WHERE user_id IN (SELECT user_id FROM public."STS_Manager" WHERE sts_id = $1)`;
   const result = await pool.query(query, [sts_id]);
   return result.rows;
 };
 
-const isManagerOfSTS = async (sts_id, user_id) => {
+modules.isManagerOfSTS = async (sts_id, user_id) => {
   const query = `SELECT 1 FROM public."STS_Manager" WHERE sts_id = $1 AND user_id = $2`;
   const result = await pool.query(query, [sts_id, user_id]);
   return result.rows.length > 0;
 };
 
-const removeManagerFromSTS = async (sts_id, user_id) => {
+modules.removeManagerFromSTS = async (sts_id, user_id) => {
   const query = `DELETE FROM public."STS_Manager" WHERE sts_id = $1 AND user_id = $2`;
   const values = [sts_id, user_id];
   await pool.query(query, values);
 };
+// =====================
+/// vechile assignment 
+// =====================   
 
-const addEntryToSTS = async (sts_id, manager_id, entry_time, vehicle_id) => {
+
+// =====================
+// STS Entry and Departure
+// =====================
+modules.addEntryToSTS = async (sts_id, manager_id, entry_time, vehicle_id) => {
   const query = `INSERT INTO public."STS_Entry" (sts_id, manager_id, entry_time, vehicle_id, volume) VALUES ($1, $2, $3, $4, 0)`;
   const values = [sts_id, manager_id, new Date(entry_time), vehicle_id];
   await pool.query(query, values);
 };
 
-const getOnlyEntriesOfSTS = async (sts_id) => {
+modules.getOnlyEntriesOfSTS = async (sts_id) => {
   const query = `SELECT * FROM public."STS_Entry" WHERE sts_id = $1 AND departure_time IS NULL`;
   const result = await pool.query(query, [sts_id]);
   return result.rows;
 };
 
-const getEntriesOfSTS = async (sts_id) => {
+modules.getEntriesOfSTS = async (sts_id) => {
   const query = `SELECT * FROM public."STS_Entry" WHERE sts_id = $1 ORDER BY entry_time DESC`;
   const result = await pool.query(query, [sts_id]);
   return result.rows;
 };
 
-const addDepartureToSTS = async (
+modules.addDepartureToSTS = async (
   sts_entry_id,
   sts_id,
   manager_id,
@@ -162,41 +171,23 @@ const addDepartureToSTS = async (
   await pool.query(query, values);
 };
 
-const addDumpEntryToSTS = async (sts_id, manager_id, entry_time, volume) => {
+modules.addDumpEntryToSTS = async (sts_id, manager_id, entry_time, volume) => {
   const query = `INSERT INTO public."STS_Entry" (sts_id, manager_id, entry_time, volume) VALUES ($1, $2, $3, $4)`;
   const values = [sts_id, manager_id, new Date(entry_time), volume];
   await pool.query(query, values);
 };
 
-const getVehiclesOfSTS = async (sts_id) => {
+modules.getVehiclesOfSTS = async (sts_id) => {
   const query = `SELECT * FROM public."Vehicle" WHERE sts_id = $1 and disabled = false`;
   const result = await pool.query(query, [sts_id]);
   return result.rows;
 };
 
-const getSTSIDfromManagerID = async (manager_id) => {
+modules.getSTSIDfromManagerID = async (manager_id) => {
   const query = `SELECT sts_id FROM public."STS_Manager" WHERE user_id = $1`;
   const result = await pool.query(query, [manager_id]);
   if (result.rows.length === 0) return null;
   return result.rows[0].sts_id;
 };
 
-module.exports = {
-  createSTS,
-  existsSTS,
-  getSTSs,
-  getSTS,
-  updateSTS,
-  deleteSTS,
-  addManagerToSTS,
-  isManagerOfSTS,
-  getManagersOfSTS,
-  removeManagerFromSTS,
-  addEntryToSTS,
-  getEntriesOfSTS,
-  getOnlyEntriesOfSTS,
-  addDepartureToSTS,
-  addDumpEntryToSTS,
-  getVehiclesOfSTS,
-  getSTSIDfromManagerID,
-};
+module.exports = modules;
