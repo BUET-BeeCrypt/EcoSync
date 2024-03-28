4405e70cae0138071274516148ff97f7c1df87c2
Tamim Ehsan <tamimehsan99@gmail.com>
2024-03-28 16:50:29 +0600

    feat: add entry for landfill

--- backend/src/api/landfill/controller.js
+++ backend/src/api/landfill/controller.js
@@ -56,4 +56,58 @@ modules.removeManagerFromLandfill = async (req, res) => {
   res.status(200).json({ message: "Manager removed from landfill" });
 }
 
+
+modules.addEntryToLandfill = async (req, res) => {
+  const manager_id = req.user.user_id;
+  
+  const landfill_id = await repository.getLandfillIdfromManagerId(manager_id);
+
+  if( landfill_id === null){
+    return res.status(404).json({ message: "Manager is not assigned to any landfill" });
+  }
+
+  const { entry_time, vehicle_id, weight } = req.body;
+
+  await repository.addEntryToLandfill(landfill_id, manager_id, entry_time, vehicle_id, weight);
+  res.status(201).json(createdLandfillEntry);
+}
+
+modules.getEntriesOfLandfill = async (req, res) => {
+  const landfill_id = await repository.getLandfillIdfromManagerId(req.user.user_id);
+
+  if( landfill_id === null){
+    return res.status(404).json({ message: "Manager is not assigned to any landfill" });
+  }
+
+  const entries = await repository.getEntriesOfLandfill(landfill_id);
+  res.status(200).json(entries);
+}
+
+modules.getOnlyEntriesOfLandfill = async (req, res) => {
+  const landfill_id = await repository.getLandfillIdfromManagerId(req.user.user_id);
+
+  if( landfill_id === null){
+    return res.status(404).json({ message: "Manager is not assigned to any landfill" });
+  }
+
+  const entries = await repository.getOnlyEntriesOfLandfill(landfill_id);
+  res.status(200).json(entries);
+}
+
+modules.addDepartureToLandfill = async (req, res) => {
+  const { landfill_entry_id } = req.params;
+  const { departure_time } = req.body;
+
+  const manager_id = req.user.user_id;
+  const landfill_id = await repository.getLandfillIdfromManagerId(manager_id);
+
+  if( landfill_id === null){
+    return res.status(404).json({ message: "Manager is not assigned to any landfill" });
+  }
+
+  await repository.addDepartureToLandfill(landfill_entry_id, departure_time);
+  res.status(200).json({ message: "Departure added to landfill" });
+}
+
+
 module.exports = modules;
